bfc6434b7ffa53434c6372896b505d75bd3a02be
Christopher <christopher@clatza.dev>
2024-05-10 17:37:18 +0200

    cleanup imports

--- src/client/java/dev/clatza/mcautofight/GlobalData.java
+++ src/client/java/dev/clatza/mcautofight/GlobalData.java
@@ -2,7 +2,6 @@ package dev.clatza.mcautofight;
 
 import dev.clatza.mcautofight.Utils.TimedRemovalList;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.pathing.Path;
 
 public class GlobalData {
     public static boolean isAttacking = false;
