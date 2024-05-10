78f6677ee365da559f900c94b7ea8a8ff8a5f2e7
Christopher <christopher@clatza.dev>
2024-05-10 17:36:42 +0200

    remove some typos, unused code and remove warning

--- src/client/java/dev/clatza/mcautofight/Utils/TimedRemovalList.java
+++ src/client/java/dev/clatza/mcautofight/Utils/TimedRemovalList.java
@@ -3,15 +3,14 @@ package dev.clatza.mcautofight.Utils;
 import java.util.concurrent.*;
 
 public class TimedRemovalList {
-    private ConcurrentHashMap<Integer, ScheduledFuture<?>> map = new ConcurrentHashMap<>();
-    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    private final ConcurrentHashMap<Integer, ScheduledFuture<?>> map = new ConcurrentHashMap<>();
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
     public void add(Integer value) {
         remove(value);
 
         ScheduledFuture<?> scheduledFuture = executorService.schedule(() -> {
             map.remove(value);
-            System.out.println("Eintrag entfernt: " + value);
         }, 2, TimeUnit.MINUTES);
 
         map.put(value, scheduledFuture);
@@ -26,9 +25,5 @@ public class TimedRemovalList {
 
     public boolean contains(Integer value) {
         return map.containsKey(value);
-    }
-
-    public void shutdown() {
-        executorService.shutdownNow();
     }
 }
