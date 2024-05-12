54774baec6ad21241951139fc1b05857c9b953cd
Christopher <christopher@clatza.dev>
2024-05-12 12:26:08 +0200

    add debug setting

--- src/client/java/dev/clatza/mcautofight/GlobalData.java
+++ src/client/java/dev/clatza/mcautofight/GlobalData.java
@@ -4,6 +4,8 @@ import dev.clatza.mcautofight.Utils.TimedRemovalList;
 import net.minecraft.entity.Entity;
 
 public class GlobalData {
+    public static boolean DEBUG = true;
+
     public static boolean isAttacking = false;
     public static Entity currentTargetEntity = null;
     public static long lastEnemyFoundAt = 0;
